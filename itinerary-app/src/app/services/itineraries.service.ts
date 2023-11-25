a325b1dfd37a7ed27bc18f10ef84fc0485016f81
EstebanCabreraArbizu <u202014600@upc.edu.pe>
2023-11-25 09:00:09 -0500

    actualización de token

--- itinerary-app/src/app/services/itineraries.service.ts
+++ itinerary-app/src/app/services/itineraries.service.ts
@@ -10,7 +10,7 @@ import { Sharing } from '../models/Sharing';
 export class ItinerariesService {
   private baseURL:string = "http://localhost:8080/api/v1/itineraries";
   private token =
-    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuaXRvQGdtYWlsLmNvbSIsImlhdCI6MTcwMDg3ODkxNCwiZXhwIjoxNzAwODgwMzU0fQ.CwM70axi_Hl0D6sCn_GF_n0Go11xoKhsRn1lfC3IOe0'; 
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuTG9yZEBnbWFpbC5jb20iLCJpYXQiOjE3MDA5MTk0NjUsImV4cCI6MTcwMDkyMDkwNX0.wjakdGi8B0rAQRX6biD7En6ELfPD4hND8Djfwn3ihmw'; 
   constructor(private http:HttpClient){}
   //Las funciones deben tener la misma estructura o similar a nuestro controller de nuestro backend
   createItinerary(resource:Itineraries): Observable<ItinerariesResponse[]>{
