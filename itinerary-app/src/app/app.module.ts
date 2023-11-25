74db4bcf1fd0f212f661e14a592351bc7fb97f48
EstebanCabreraArbizu <u202014600@upc.edu.pe>
2023-11-24 19:58:34 -0500

    mejora del navbar y uso de angular material e importacion de flex.io

--- itinerary-app/src/app/app.module.ts
+++ itinerary-app/src/app/app.module.ts
@@ -10,20 +10,33 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ActivityModule } from './components/activity/activity.module';
 import { ItineraryModule } from './components/itinerary/itinerary.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'; // Import MatFormFieldModule
+import { MatInputModule } from '@angular/material/input'; // Import MatInputModule
+import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FooterComponent,
     NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     ActivityModule,
-    ItineraryModule
+    ItineraryModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
